26dd382100834cd5130bf2cc8c2099386cdba23a
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2023-06-29 18:10:03 +1000

    start thinking about sublists

--- todo-list.lisp
+++ todo-list.lisp
@@ -3,24 +3,28 @@
 (require :uiop)
 
 (defvar *database* ())
-(defvar *current-database* "")
+(defvar *current-database-name* "")
 
 (defparameter *db-path* (concatenate 'string
                           (namestring *default-pathname-defaults*)
                           "db/"))
 
+(defmacro coerce-to-string (name)
+  (unless (typep name 'string)
+    (setf name (format nil "~a" name))))
 
 (defun load-database (database-name)
-  (with-open-file (file (concatenate 'string *db-path* database-name)
-                        :direction :input
-                        :if-does-not-exist nil)
-    (setf *database* (read-line file))
-    (setf *current-database* database-name)))
+  (coerce-to-string database-name)
 
-(defun save-database (&optional (database-name *current-database*))
+  (with-open-file (file (concatenate 'string *db-path* database-name))
+    :direction :input
+    :if-does-not-exist :file-error
+    (setf *database* (read-line file))
+    (setf *current-database-name* database-name)))
+
+(defun save-database (&optional (database-name *current-database-name*))
   ; Ensure `database-name` is a string
-  (unless (typep database-name 'string)
-    (setf database-name (format nil "~a" database-name)))
+  (coerce-to-string database-name)
 
   (ensure-directories-exist *db-path*)
 
@@ -33,9 +37,20 @@
 (defun clear-database ()
   (setf *database* nil))
 
-(defun add-todo (name &optional (description "(unspecified)"))
-  (setf *database*
-    (append (list (list name description)) *database*)))
+;; Add a new todo entry to `*database*`. 
+;; `name`: a 
+(defun add-todo (name &optional (description (symbol-name name)))
+  "Adds a new todo entry to `*database*`. 
+  `name` may be a symbol, or a list of symbols.
+  "
+  ; entries consist of a name, an optional description, and a list of sub-entries
+  (typecase name
+    (symbol
+     (setf *database*
+       (push (list name description nil) *database*)))
+    (list
+     ; TODO: implement. traverse down the list `name`, constructing new todos as needed.
+     )))
 
 (defun done-todo (name)
   (setf *database*
