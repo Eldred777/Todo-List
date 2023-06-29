cedd29ddcf57dfa0306b84a34abaf0069d163c1e
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2023-06-29 15:03:29 +1000

    first prototype done

--- todo-list.lisp
+++ todo-list.lisp
@@ -1,34 +1,46 @@
-(defpackage :todo-list
-  )
+(defpackage :todo-list)
 
 (require :uiop)
 
 (defvar *database* ())
+(defvar *current-database* "")
 
-(defun create-new-database (database-name)
-  ; Check type of database-name. Should be a string. 
-  ; Check if database already exists first
-  (uiop:probe-file* database-name)
-  ; TODO: implement 
-  )
+(defparameter *db-path* (concatenate 'string
+                          (namestring *default-pathname-defaults*)
+                          "db/"))
+
 
 (defun load-database (database-name)
-  ; TODO: implement 
-  ; Check if database already exists, else fail gracefully and return nil.
-  )
+  (with-open-file (file (concatenate 'string *db-path* database-name)
+                        :direction :input
+                        :if-does-not-exist nil)
+    (setf *database* (read-line file))
+    (setf *current-database* database-name)))
 
-(defun save-database (database-name)
-  ; TODO: implement 
-  )
+(defun save-database (&optional (database-name *current-database*))
+  ; Ensure `database-name` is a string
+  (unless (typep database-name 'string)
+    (setf database-name (format nil "~a" database-name)))
 
-(defun add-todo (name)
-  ; TODO: implement 
-  )
+  (ensure-directories-exist *db-path*)
+
+  (with-open-file (file (concatenate 'string *db-path* database-name)
+                        :direction :output
+                        :if-exists :supersede
+                        :if-does-not-exist :create)
+    (format file "~a" *database*)))
+
+(defun clear-database ()
+  (setf *database* nil))
+
+(defun add-todo (name &optional (description "(unspecified)"))
+  (setf *database*
+    (append (list (list name description)) *database*)))
 
 (defun done-todo (name)
-  ; TODO: implement 
-  )
+  (setf *database*
+    (remove-if (lambda (x) (eql (car x) name))
+        *database*)))
 
 (defun show-todo ()
-  ; TODO: implement
-  )
+  (format t "~{~{~a: ~a~}~%~}" *database*))
