dbd78feff1b949cf82dd395cba6b30d179cb2dc3
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2023-07-01 01:16:35 +1000

    major functionality of adding todos done

--- todo-list.lisp
+++ todo-list.lisp
@@ -6,10 +6,11 @@
   (not (null l)))
 
 ;; Entries to `*database*` are 
-;; entry = (name: symbol, description: string, sublist: list[entry])
+;; item = (id: symbol, description: string, sublist: list[entry])
 (defvar *database* ())
-(defun get-todo-sublist (entry)
-  (caddr entry))
+(defun get-todo-id (entry) (car entry))
+(defun get-todo-description (entry) (cadr entry))
+(defun get-todo-sublist (entry) (caddr entry))
 
 (defvar *current-database-name* "")
 
@@ -48,80 +49,89 @@
 (defun position-of-todo (name target-list)
   (position (car name) (map 'list #'first target-list)))
 
-(defun construct-todo (target-list name &optional (description "_"))
+(defun construct-todo (target-list entry-id &optional (description "_"))
   ; TODO: implement. constructs a todo recursively in a functional style,
-  ; returning a copy of `target-list` with a new todo in `name` with
+  ; returning a copy of `target-list` with a new todo in `entry-id` with
   ; `description` and `sublist`=`nil`. 
 
   (coerce-to-string description)
 
-  (etypecase name
-    (symbol (cons (list name description nil) target-list)) ; [x] test
-    (list ; [ ] test
-         ; TODO: verification that all entries of list are symbols, else error
-         ; TODO: below still not working. 
-         (if (> (length name) 1)
-             (if (not-null target-list)
-                 ; if nil then we have created a new list in the previous step
-                 ; so just create the next entry. 
-                 (cons
-                   (list (car name)
-                         "_"
-                         ; Handle terminal case, we don't want a '(())
-                         (let ((sublist (construct-todo nil (cdr name) description)))
-                           (if (null sublist)
-                               nil
-                               (list sublist))))
-                   target-list) ; TODO: remove repeated code, possibly via lazy evaluation? 
-                 ; else, we need to find the appropriate entry to traverse into,
-                 ; if any such entry exists. 
-                 (let ((index (position (car name) (map 'list #'first target-list))))
-                   (if (numberp index)
-                       ; If exists, simply traverse down and run again
-                       (construct-todo (get-todo-sublist (nth index target-list))
-                                       (cdr name) description)
-                       ; Otherwise we need to add a new entry
-                       (cons
-                         (list (car name)
-                               "_"
-                               ; Handle terminal case, we don't want a '(())
-                               (let ((sublist (construct-todo nil (cdr name) description)))
-                                 (if (null sublist)
-                                     nil
-                                     (list sublist))))
-                         target-list))))
-             ; if nil, just return nil which is handled explicitly below
-             nil))))
+  (etypecase entry-id
+    ; [x] test
+    (symbol (if (null entry-id)
+                ; if nil, just return nil which is handled explicitly below
+                nil
+                ; verify that this is not in the list 
+                (cons (list entry-id description nil) target-list)))
+    ; [ ] test
+    (list
+     ; TODO: verification that all entries of list are symbols, else error
+     ; TODO: below still not working. 
+     (if (null target-list)
+         ; if nil then we have created a new list in the previous step
+         ; so just create the next entry. 
+         (cons
+           (list (get-todo-id entry-id)
+                 "_" ; TODO (simultaneous) handle final entry entry-id 
+                 ; Handle terminal case, we don't want a '(())
+                 (let ((sublist (construct-todo nil (cdr entry-id) description)))
+                   (if (null sublist)
+                       nil
+                       sublist)))
+           target-list)
+         ; TODO: remove repeated code, possibly via lazy evaluation? 
+         ; else, we need to find the appropriate entry to traverse into,
+         ; if any such entry exists. 
+         (let ((index (position (get-todo-id entry-id) (map 'list #'first target-list))))
+           (if (numberp index)
+               ; If exists, simply traverse down and run again
+               (let ((list (nth index target-list))
+                     (sublist (get-todo-sublist (nth index target-list))))
+                 (cons (list (get-todo-id entry-id)
+                             "_" ; TODO (simultaneous) handle final entry entry-id
+                             (construct-todo sublist (cdr entry-id) description))
+                       (remove list target-list)))
+               ; TODO: is there a better way to do this `remove list` (see below)
+               ; TODO: is there a way to do the above let block via mutation instead? 
+               ; Otherwise we need to add a new entry
+               (cons (list (get-todo-id entry-id)
+                           "_" ; TODO (simultaneous) handle final entry entry
+                           ; Handle terminal case, we don't want a '(())
+                           (let ((sublist (construct-todo nil (cdr entry-id) description)))
+                             (if (null sublist)
+                                 nil
+                                 sublist)))
+                     target-list)))))))
 
 ;; TODO: test 
-(defun add-todo (name &optional (description "_"))
+(defun add-todo (entry &optional (description "_"))
   "Adds a new todo entry to `*database*`. Defaults to a blank description if none is provided.
-  `name` may be a symbol, or a list of symbols.
+  `entry` may be a symbol, or a list of symbols.
   `description` should be a string that describes the todo entry 
   "
-  (setf *database* (construct-todo *database* name description)))
+  (setf *database* (construct-todo *database* entry description)))
 
-(defun remove-todo (target-list name)
-  ; TODO: implement. returns a copy of `target-list` with `name` removed
+(defun remove-todo (target-list entry)
+  ; TODO: implement. returns a copy of `target-list` with `entry` removed
+  entry
+  target-list
+  ; shut up compiler
   )
 
+(defun done-todo (entry)
+  (setf *database* (remove-todo *database* entry)))
 
-(defun done-todo (name)
-  (setf *database*
-    (remove-if (lambda (x) (eql (car x) name))
-        *database*)))
-
-(defun rename-todo (name new-name)
+(defun rename-todo (entry new-name)
   "Rename the symbol used for a todo."
   ; TODO: implement 
-  name
+  entry
   new-name)
 
-(defun describe-todo (name description)
+(defun describe-todo (entry description)
   "Change the description used for a todo."
   ; TODO: implement 
   ; TODO: option to only update if blank 
-  name
+  entry
   description)
 
 (defun show-todo ()
